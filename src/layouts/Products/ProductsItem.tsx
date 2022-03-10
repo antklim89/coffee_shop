1ca3717d4db56d2ec8a22a6c2ed8e537ea54d0fe
Anton <antklim89@mail.ru>
2022-03-10 20:35:44 +0300

    added placeholder image

--- src/layouts/Products/ProductsItem.tsx
+++ src/layouts/Products/ProductsItem.tsx
@@ -1,5 +1,6 @@
-import { LinkBox, Heading, Text, LinkOverlay } from '@chakra-ui/react';
+import { LinkBox, Heading, Text, LinkOverlay, Box } from '@chakra-ui/react';
 import { Link } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import { FC } from 'react';
 
 import { ProductsItemProps } from './Products.types';
@@ -12,21 +13,26 @@ const ProductsItem: FC<ProductsItemProps> = ({ product }) => {
             as="section"
             backgroundColor="whiteAlpha.600"
             minH={32}
-            p={2}
             shadow="lg"
         >
-            <Heading as="h2" size="md">
-                <LinkOverlay
-                    as={Link}
-                    href={`/product/${product.id}`} 
-                    to={`/product/${product.id}`}
-                >
-                    {product.title}
-                </LinkOverlay>
-            </Heading>
-            <Text fontSize="2xl" fontWeight="bold">${product.variants[0].price}</Text>
-            <Text fontSize="sm" fontWeight="bold">{product.variants[0].weight} {product.variants[0].weightUnit}</Text>
-            {product.description}
+            {/* Placeholder */}
+            <StaticImage alt="A dinosaur" src='../../assets/placeholder.jpg' />
+            {/*  */}
+            <Box p={2}>
+                <Heading as="h2" size="md">
+                    <LinkOverlay
+                        as={Link}
+                        href={`/product/${product.id}`} 
+                        to={`/product/${product.id}`}
+                    >
+                        {product.title}
+                    </LinkOverlay>
+                </Heading>
+                <Text fontSize="2xl" fontWeight="bold">${product.variants[0].price}</Text>
+                <Text fontSize="sm" fontWeight="bold">{product.variants[0].weight} {product.variants[0].weightUnit}</Text>
+                {product.description}
+
+            </Box>
         </LinkBox>
     );
 };
